60f62c85b43fc2e0890d2f78d71a304367cfb6a7
Adrián Mugnolo <adrian@mugnolo.com>
2017-05-27 12:12:31 -0400

    Fix assertion in test
    Make test suite pass again.

--- apps/montreal_elixir_web/test/controllers/page_controller_test.exs
+++ apps/montreal_elixir_web/test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MontrealElixir.Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Montreal Elixir!"
   end
 end
