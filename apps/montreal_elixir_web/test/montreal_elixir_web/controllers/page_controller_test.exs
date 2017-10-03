8f563a9f019e2a5c42b714fefa668b132487bbf7
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-03 04:58:13 -0400

    test: added spec for showing next meetup section
    All tests pass, but page only shows static text from mockup. The next
    step will be fetching data from meetup.com API.

--- apps/montreal_elixir_web/test/montreal_elixir_web/controllers/page_controller_test.exs
+++ apps/montreal_elixir_web/test/montreal_elixir_web/controllers/page_controller_test.exs
@@ -6,5 +6,14 @@ defmodule MontrealElixirWeb.PageControllerTest do
       conn = get conn, "/"
       assert html_response(conn, 200) =~ "About Montreal Elixir"
     end
+
+    test "shows the next meetup from Meetup.com", %{conn: conn} do
+      conn = get conn, "/"
+      resp = html_response(conn, 200)
+
+      assert resp =~ "Next event"
+      assert resp =~ "Wed June 14 at 6:30 PM"
+      assert resp =~ "490 rue de la Gauchetiere Ouest"
+    end
   end
 end
