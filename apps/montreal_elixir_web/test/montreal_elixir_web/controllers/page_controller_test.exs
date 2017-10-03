f9b52ef521c33960a7f955d15dad0e0ded02e994
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-03 04:52:32 -0400

    test: refactored GET spec into decribe block

--- apps/montreal_elixir_web/test/montreal_elixir_web/controllers/page_controller_test.exs
+++ apps/montreal_elixir_web/test/montreal_elixir_web/controllers/page_controller_test.exs
@@ -1,8 +1,10 @@
 defmodule MontrealElixirWeb.PageControllerTest do
   use MontrealElixirWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "About Montreal Elixir"
+  describe "GET /" do
+    test "shows About section", %{conn: conn} do
+      conn = get conn, "/"
+      assert html_response(conn, 200) =~ "About Montreal Elixir"
+    end
   end
 end
