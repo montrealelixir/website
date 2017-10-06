8fc0988b0d86345dd89565ad89f8bcd9341859fd
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-06 06:00:52 -0400

    test: added simple test for Cache.fetch/3

---
+++ apps/montreal_elixir/test/montreal_elixir/social_feeds/cache_test.exs
@@ -0,0 +1,13 @@
+defmodule MontrealElixir.SocialFeeds.CacheTest do
+  use ExUnit.Case, async: true
+
+  alias MontrealElixir.SocialFeeds.Cache
+
+  describe "fetch/3" do
+    test "returns the result of the default function" do
+      default_function = fn -> 1 end
+
+      assert Cache.fetch(:my_key, default_function, %{}) == 1
+    end
+  end
+end
