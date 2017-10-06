6b255194a6d19f67866aa564519560feb63d013d
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-06 07:26:35 -0400

    feat: added simple caching in a Map

--- apps/montreal_elixir/lib/montreal_elixir/social_feeds/cache.ex
+++ apps/montreal_elixir/lib/montreal_elixir/social_feeds/cache.ex
@@ -7,16 +7,35 @@ defmodule MontrealElixir.SocialFeeds.Cache do
   Starts the cache.
   """
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, [], opts)
+    GenServer.start_link(__MODULE__, %{}, opts)
   end
 
   def fetch(key, default_value_function, _opts) do
-    GenServer.call(__MODULE__, {:set, key, default_value_function.()})
+    case get(key) do
+      {:not_found} -> set(key, default_value_function.())
+      {:found, result} -> result
+    end
+  end
+
+  defp get(key) do
+    case GenServer.call(__MODULE__, {:get, key}) do
+      :error -> {:not_found}
+      {:ok, result} -> {:found, result}
+    end
+  end
+
+  defp set(key, value) do
+    GenServer.call(__MODULE__, {:set, key, value})
   end
 
   ## Server Callbacks
 
-  def handle_call({:set, _key, value}, _from, state) do
+  def handle_call({:get, key}, _from, state) do
+    {:reply, Map.fetch(state, key), state}
+  end
+
+  def handle_call({:set, key, value}, _from, state) do
+    state = Map.put(state, key, value)
     {:reply, value, state}
   end
 end
--- apps/montreal_elixir/test/montreal_elixir/social_feeds/cache_test.exs
+++ apps/montreal_elixir/test/montreal_elixir/social_feeds/cache_test.exs
@@ -3,11 +3,17 @@ defmodule MontrealElixir.SocialFeeds.CacheTest do
 
   alias MontrealElixir.SocialFeeds.Cache
 
-  describe "fetch/4" do
+  describe "fetch/3" do
     test "returns the result of the default function" do
       default_function = fn -> 1 end
 
-      assert Cache.fetch(:my_key, default_function, %{}) == 1
+      assert Cache.fetch(:new_key, default_function, %{}) == 1
+    end
+
+    test "returns the cached result if available" do
+      Cache.fetch(:cached_key, fn -> 2 end, %{})
+
+      assert Cache.fetch(:cached_key, fn -> :dont_return_this end, %{}) == 2
     end
   end
 end
