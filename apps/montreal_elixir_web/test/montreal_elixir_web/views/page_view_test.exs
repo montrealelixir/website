ece1420eefe01092f3e2f892e40169e3d5123ada
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-08 01:53:03 -0400

    fix: handling case where meetup feed returns nothing

--- apps/montreal_elixir_web/test/montreal_elixir_web/views/page_view_test.exs
+++ apps/montreal_elixir_web/test/montreal_elixir_web/views/page_view_test.exs
@@ -1,3 +1,11 @@
 defmodule MontrealElixirWeb.PageViewTest do
   use MontrealElixirWeb.ConnCase, async: true
+  import Phoenix.View
+
+  test "renders index.html if no next_meetup", %{conn: conn} do
+    content = render_to_string(MontrealElixirWeb.PageView, "index.html", conn: conn, next_meetup: nil)
+
+    assert String.contains?(content, "About Montreal Elixir")
+    refute String.contains?(content, "Next event")
+  end
 end
