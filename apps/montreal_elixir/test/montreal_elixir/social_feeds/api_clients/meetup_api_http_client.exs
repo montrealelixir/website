f57dfc28b5a4d69b22ee8632d319c2b12b1a65bc
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-08 01:26:35 -0400

    refac: renamed fixture file for single upcoming event

--- apps/montreal_elixir/test/montreal_elixir/social_feeds/api_clients/meetup_api_http_client.exs
+++ apps/montreal_elixir/test/montreal_elixir/social_feeds/api_clients/meetup_api_http_client.exs
@@ -1,13 +1,13 @@
 defmodule MontrealElixir.SocialFeeds.Test.MeetupApiHTTPClient do
   @moduledoc false
 
-  @meetup_json File.read!("test/montreal_elixir/fixtures/meetup_upcoming_events.json")
+  @upcoming_meetups_json File.read!("test/montreal_elixir/fixtures/meetup_get_upcoming_events.json")
   @meetups_json File.read!("test/montreal_elixir/fixtures/meetup_get_events.json")
 
   def request(url) do
     url = to_string(url)
     cond do
-      String.contains?(url, "page=1") -> {:ok, {[], [], @meetup_json}}
+      String.contains?(url, "scroll=future_or_past") -> {:ok, {[], [], @upcoming_meetups_json}}
       String.contains?(url, "error=true") -> {:error, {}}
       true -> {:ok, {[], [], @meetups_json}}
     end
