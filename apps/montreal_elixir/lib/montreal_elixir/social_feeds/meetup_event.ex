148088ec55679bd8fcf153a130016b6eef31413b
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-09 12:50:05 -0400

    doc: fixed moduledoc for MeetupEvent struct

--- apps/montreal_elixir/lib/montreal_elixir/social_feeds/meetup_event.ex
+++ apps/montreal_elixir/lib/montreal_elixir/social_feeds/meetup_event.ex
@@ -1,3 +1,6 @@
 defmodule MontrealElixir.SocialFeeds.MeetupEvent do
+  @moduledoc """
+  Struct keeping meetup event attributes.
+  """
   defstruct [:name, :utc_datetime, :venue_name, :venue_address, :url]
 end
