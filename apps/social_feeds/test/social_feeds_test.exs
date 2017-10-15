335d787b09c8b1b8dc8c968754b78a0b53bcf095
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-15 15:06:30 -0400

    test: fixed top level app API test

--- apps/social_feeds/test/social_feeds_test.exs
+++ apps/social_feeds/test/social_feeds_test.exs
@@ -1,8 +1,21 @@
 defmodule SocialFeedsTest do
-  use ExUnit.Case
-  doctest SocialFeeds
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert SocialFeeds.hello() == :world
+  alias SocialFeeds.Meetup.Event
+
+  describe "get_next_meetup_event/0" do
+    @tag :capture_log
+    test "returns the next meetup_event" do
+      {:ok, expected_time, _offset} = DateTime.from_iso8601("2017-10-11 22:30:00Z")
+      event = %Event{
+        name: "Montreal Elixir Meetup",
+        utc_datetime: expected_time,
+        venue_name: "Shopify Montreal",
+        venue_address: "490 rue de la Gauchetiere Ouest",
+        url: "https://www.meetup.com/montrealelixir/events/243582390/"
+      }
+
+      assert SocialFeeds.get_next_meetup_event() == event
+    end
   end
 end
