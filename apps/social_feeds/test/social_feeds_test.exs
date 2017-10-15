837440df6b4196c55801e07cef0c8915a283453f
Kamil Politowicz <k.politowicz@gmail.com>
2017-10-15 15:20:04 -0400

    test: added "value from cache" test
    Also defined Cache.clear fn - it is now used only in test, but from the
    cache API perspective it makes sense to define it as well.

--- apps/social_feeds/test/social_feeds_test.exs
+++ apps/social_feeds/test/social_feeds_test.exs
@@ -1,13 +1,17 @@
 defmodule SocialFeedsTest do
   use ExUnit.Case, async: true
 
-  alias SocialFeeds.Meetup.Event
+  alias SocialFeeds.Cache
 
   describe "get_next_meetup_event/0" do
+    setup do
+      Cache.clear
+    end
+
     @tag :capture_log
     test "returns the next meetup_event" do
       {:ok, expected_time, _offset} = DateTime.from_iso8601("2017-10-11 22:30:00Z")
-      event = %Event{
+      event = %SocialFeeds.Meetup.Event{
         name: "Montreal Elixir Meetup",
         utc_datetime: expected_time,
         venue_name: "Shopify Montreal",
@@ -16,6 +20,12 @@ defmodule SocialFeedsTest do
       }
 
       assert SocialFeeds.get_next_meetup_event() == event
+    end
+
+    test "returns the next meetup_event from cache" do
+      Cache.fetch(:next_meetup_event, fn -> :cached_value end, %{})
+
+      assert SocialFeeds.get_next_meetup_event() == :cached_value
     end
   end
 end
