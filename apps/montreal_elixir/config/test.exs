bd4c09419f3ec9c90b8b9bdce28b9e827ea100b1
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2019-10-20 14:39:25 -0400

    Centralize config (#64)
    This makes it a lot easier to reason about.

--- apps/montreal_elixir/config/test.exs
+++
@@ -1,6 +0,0 @@
-use Mix.Config
-
-# Configure the Ecto repository
-config :montreal_elixir, MontrealElixir.Repo,
-  database: "montreal_elixir_test",
-  pool: Ecto.Adapters.SQL.Sandbox
--- config/staging.exs
+++ config/staging.exs
@@ -2,3 +2,11 @@ use Mix.Config
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+###############################################################################
+# MontrealElixirWeb
+###############################################################################
+
+config :montreal_elixir_web, MontrealElixirWeb.Endpoint,
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://*.herokuapp.com"]
