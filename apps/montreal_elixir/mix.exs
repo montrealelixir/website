bc3c0457d4608bca9759a96a846b2b1710a58b28
Nicholas Henry <nicholas.henry@gmail.com>
2019-10-27 11:28:55 -0400

    fix: Explicitly start SSL

--- apps/montreal_elixir/mix.exs
+++ apps/montreal_elixir/mix.exs
@@ -23,7 +23,7 @@ defmodule MontrealElixir.Mixfile do
   def application do
     [
       mod: {MontrealElixir.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
