cd693b04ffafa6b394a61ec0562df99ce0368048
Nicholas Henry <nicholas.henry@gmail.com>
2023-02-05 11:13:45 -0500

    fix: Use the search feed to get videos
    The activity feed includes the creation of playlists which we don't want.
    Also, the shape of a playlist in the activity feed is different to
    that of the video object, causing the parsing logic to crash.

--- apps/social_feeds/test/api_clients/youtube_api_http_client.exs
+++ apps/social_feeds/test/api_clients/youtube_api_http_client.exs
@@ -1,7 +1,7 @@
 defmodule SocialFeeds.Test.YoutubeApiHttpClient do
   @moduledoc false
 
-  @activities_json File.read!("test/fixtures/youtube_get_activities.json")
+  @search_json File.read!("test/fixtures/youtube_get_search.json")
 
   def request(url) do
     url = to_string(url)
@@ -9,7 +9,7 @@ defmodule SocialFeeds.Test.YoutubeApiHttpClient do
     if String.contains?(url, "error=true") do
       {:error, {}}
     else
-      {:ok, {{"HTTP/1.1", 200, "OK"}, [], @activities_json}}
+      {:ok, {{"HTTP/1.1", 200, "OK"}, [], @search_json}}
     end
   end
 end
